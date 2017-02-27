1cebd02b5d09cd415a9059fa25f2c5bbae2b359e
Juan Gualberto <jgutierrez@iesvirgendelcarmen.com>
2017-02-27 20:06:25 +0100

    Corrección de errores en mensajes y añadidos comentarios.

--- www/lib/alumno.js
+++ www/lib/alumno.js
@@ -1,57 +1,54 @@
 /*jslint browser:true, devel:true, white:true, vars:true */
 /*global $:false, intel:false */
 // variables para el jslint
+
+/**
+* Creamos el objeto alumno y todos sus métodos.
+*/
 $.alumno={};
 // Configuración del HOST y URL del servicio
 $.alumno.HOST = 'http://localhost:8080';
 // $.alumno.URL = '/GA-JPA/webresources/com.iesvdc.acceso.entidades.alumno';
 $.alumno.URL = '/GAREST/webresources/com.iesvdc.acceso.entidades.alumno';
 
-$.alumno.AlumnoReadREST = function(id) {
-    if ( id === undefined ) {
-        $.ajax({
-            url: this.HOST+this.URL,
-            type: 'GET',
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function (json) {
-                $('#r_alumno').empty();
-                $('#r_alumno').append('<h3>Listado de Alumnos</h3>');
-                var table = $('<table />').addClass('table table-stripped');
 
-                table.append($('<thead />').append($('<tr />').append('<th>id</th>', '<th>nombre</th>', '<th>apellidos</th>')));
-                var tbody = $('<tbody />');
-                for (var clave in json) {
-                    tbody.append($('<tr />').append('<td>' + json[clave].id + '</td>',
-                                '<td>' + json[clave].nombre + '</td>', '<td>' + json[clave].apellido + '</td>'));
-                }
-                table.append(tbody);
+/**
+    Esta función hace la llamada REST al servidor y crea la tabla con todos los alumnos.
+*/
+$.alumno.AlumnoReadREST = function() {
+    // con esta función jQuery hacemos la petición GET que hace el findAll()
+    $.ajax({
+        url: this.HOST+this.URL,
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (json) {
+            $('#r_alumno').empty();
+            $('#r_alumno').append('<h3>Listado de Alumnos</h3>');
+            var table = $('<table />').addClass('table table-stripped');
 
-                $('#r_alumno').append( $('<div />').append(table) );
-                $('tr:odd').css('background','#CCCCCC');
-            },
-            error: function (xhr, status) {
-                $('#r_alumno').empty();
-                $('#r_alumno').append('<h3>Error conectando al servidor</h3>');
-                $('#r_alumno').append('<p>Inténtelo más tarde</p>');
+            table.append($('<thead />').append($('<tr />').append('<th>id</th>', '<th>nombre</th>', '<th>apellidos</th>')));
+            var tbody = $('<tbody />');
+            for (var clave in json) {
+                tbody.append($('<tr />').append('<td>' + json[clave].id + '</td>',
+                            '<td>' + json[clave].nombre + '</td>', '<td>' + json[clave].apellido + '</td>'));
             }
-        });
-    } else {
-        $.ajax({
-            url: 'http://localhost:8080/GA-JPA/webresources/com.iesvdc.acceso.entidades.alumno',
-            type: 'GET',
-            dataType: 'json',
-            success: function (json) {
-                
-            },
-            error: function (xhr, status) {
-                this.error('Imposible leer alumno','Compruebe su conexión e inténtelo de nuevo más tarde');
-            }
-        });
-    }
+            table.append(tbody);
+
+            $('#r_alumno').append( $('<div />').append(table) );
+            $('tr:odd').css('background','#CCCCCC');
+        },
+        error: function (xhr, status) {
+             $.alumno.error('Imposible leer alumno','Compruebe su conexión e inténtelo de nuevo más tarde');
+        }
+    });
 };
 
+/**
+    Esta función carga los datos del formulario y los envía vía POST al servicio REST
+*/
 $.alumno.AlumnoCreateREST = function(){
+    // Leemos los datos del formulario pidiendo a jQuery que nos de el valor de cada input.
     var datos = {
         'nombre' : $("#c_al_nombre").val(),
         'apellido': $("#c_al_apellidos").val()
@@ -82,6 +79,9 @@ $.alumno.AlumnoCreateREST = function(){
     
 };
 
+/**
+    Crea un desplegable, un select, con todos los alumnos del servicio para seleccionar el alumno a eliminar
+*/
 $.alumno.AlumnoDeleteREST = function(id){
     // si pasamos el ID directamente llamamos al servicio DELETE
     // si no, pintamos el formulario de selección para borrar.
@@ -115,14 +115,16 @@ $.alumno.AlumnoDeleteREST = function(id){
                 $('#d_alumno').empty();
                 var formulario = $('<div />');
                 formulario.addClass('container');
+                var div_select = $('<div />');
+                div_select.addClass('form-group');
                 var select = $('<select id="d_al_sel" />');
                 select.addClass('form-group');
                 for (var clave in json){
                     select.append('<option value="'+json[clave].id+'">'+json[clave].nombre+' ' + json[clave].apellido+'</option>');
                 }
                 formulario.append(select);
-                formulario.append('<div class="btn btn-danger" onclick="$.alumno.AlumnoDeleteREST(1)"> eliminar! </div>');
-                $('#d_alumno').append(formulario).append(select);
+                formulario.append('<div class="form-group"></div>').append('<div class="btn btn-danger" onclick="$.alumno.AlumnoDeleteREST(1)"> eliminar! </div>');
+                $('#d_alumno').append(formulario);
             },
             error: function(jqXHR, textStatus, errorThrown){
                 $.alumno.error('Error: Alumno Delete','No ha sido posible conectar al servidor. Compruebe su conexión.');
@@ -131,6 +133,13 @@ $.alumno.AlumnoDeleteREST = function(id){
     }
     
 };
+
+/**
+    Función para la gestión de actualizaciones. Hay tres partes: 
+    1) Listado 
+    2) Formulario para modificación
+    3) Envío de datos al servicio REST (PUT)
+*/
 
 $.alumno.AlumnoUpdateREST = function(id, envio){
     if ( id === undefined ) {
@@ -206,6 +215,10 @@ $.alumno.AlumnoUpdateREST = function(id, envio){
     }
 };
 
+
+/**
+    Función para la gestión de errores y mensajes al usuario
+*/
 $.alumno.error = function(title, msg){
     $('#err_alumno').empty();
     $('#err_alumno').append('<h3>'+title+'</h3>');
